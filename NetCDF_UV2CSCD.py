5af0c6b23bc1ebb6f80dcea24e555e815ea64903
shaunwbell <shaun.w.bell@gmail.com>
2017-09-26 14:42:28 -0700

    fix

--- NetCDF_UV2CSCD.py
+++ NetCDF_UV2CSCD.py
@@ -23,7 +23,7 @@ __status__   = "Development"
 
 """------------------------------- MAIN--------------------------------------------"""
 
-parser = argparse.ArgumentParser(description='Interpolate or adjust times')
+parser = argparse.ArgumentParser(description='Update Direction from U and V')
 parser.add_argument('sourcefile', metavar='sourcefile', type=str,
                help='complete path to epic file')
 parser.add_argument('-update','--update', action="store_true",
@@ -36,6 +36,7 @@ if args.update:
     ncfile = args.sourcefile
     df = EcoFOCI_netCDF(ncfile)
     global_atts = df.get_global_atts()
+    nchandle = df._getnchandle_()
     vars_dic = df.get_vars()
     ncdata = df.ncreadfile_dic()
 
@@ -45,4 +46,18 @@ if args.update:
             CD[ind,0,0,0] = 1e35
         else:
             CD[ind,0,0,0] = np.rad2deg(np.arctan2(-1*ncdata['U_320'][ind,0,0,0],-1*ncdata['V_321'][ind,0,0,0]))+180
-    df.close()
+    nchandle.variables['CD_310'][:,0,0,0] = CD[:,0,0,0]
+
+update = 'CD - direction, recalculated from mag dec corrected U/V components '
+
+if not 'History' in global_atts.keys():
+    print "adding history attribute"
+    histtime=datetime.datetime.utcnow()
+    nchandle.setncattr('History','{histtime:%B %d, %Y %H:%M} UTC - {history} '.format(histtime=histtime,history=update))
+else:
+    print "updating history attribute"
+    histtime=datetime.datetime.utcnow()
+    nchandle.setncattr('History', global_atts['History'] +'\n'+ '{histtime:%B %d, %Y %H:%M} UTC - {history}'.format(histtime=histtime,history=update))
+
+
+df.close()
