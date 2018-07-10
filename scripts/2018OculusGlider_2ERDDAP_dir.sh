c73cc326cc833a30682d5255e44471d9e33b9354
shaunwbell <shaun.bell@noaa.gov>
2018-07-10 11:31:48 -0700

    shell update

--- scripts/2018OculusGlider_2ERDDAP_dir.sh
+++ scripts/2018OculusGlider_2ERDDAP_dir.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-base_path="/home/pavlof"
-data_dir="/home/ecoraid/2018/Profilers/OculusGliders/PS_Spring/erddap/sg403/*.nc"
-prog_dir="${base_path}/bell/Programs/Python/EcoFOCI_MooringAnalysis/"
+base_path=/home/pavlof
+data_dir=/home/ecoraid/data/2018/Profilers/OculusGliders/PS_Spring/erddap/sg403/p*.nc
+prog_dir=${base_path}/bell/Programs/Python/EcoFOCI_MooringAnalysis/
 
-cp "/home/ecoraid/data/2018/Profilers/OculusGliders/PS_Spring/sg403/*.nc" \
-	"/home/ecoraid/data/2018/Profilers/OculusGliders/PS_Spring/erddap/sg403/"
+cp /home/ecoraid/data/2018/Profilers/OculusGliders/PS_Spring/sg403/p*.nc \
+	/home/ecoraid/data/2018/Profilers/OculusGliders/PS_Spring/erddap/sg403/
 
 ###
 # adds profile_id, or other to be ERDDAP/CF compliant
@@ -17,12 +17,12 @@ do
 	python ${prog_dir}EPICCFgliderprofile2ERDDAP_V2.py ${files} 'profile_id' 
 done
 
-"---"
-data_dir="/home/ecoraid/2018/Profilers/OculusGliders/BS_Summer/erddap/sg403/*.nc"
-prog_dir="${base_path}/bell/Programs/Python/EcoFOCI_MooringAnalysis/"
+###
+data_dir=/home/ecoraid/data/2018/Profilers/OculusGliders/BS_Summer/erddap/sg403/p*.nc
+prog_dir=${base_path}/bell/Programs/Python/EcoFOCI_MooringAnalysis/
 
-cp "/home/ecoraid/data/2018/Profilers/OculusGliders/BS_Summer/sg403/*.nc" \
-	"/home/ecoraid/data/2018/Profilers/OculusGliders/BS_Summer/erddap/sg403/"
+cp /home/ecoraid/data/2018/Profilers/OculusGliders/BS_Summer/sg403/p*.nc \
+	/home/ecoraid/data/2018/Profilers/OculusGliders/BS_Summer/erddap/sg403/
 
 ###
 # adds profile_id, or other to be ERDDAP/CF compliant
