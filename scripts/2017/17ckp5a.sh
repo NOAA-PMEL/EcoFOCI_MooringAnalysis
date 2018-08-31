967d399e9f2df62d5f5f99152c04f64517740d9c
shaunbell <shaun.bell@noaa.gov>
2018-08-31 11:57:36 -0700

    add more 2017 config edits

--- scripts/2017/17ckp5a.sh
+++ scripts/2017/17ckp5a.sh
@@ -29,3 +29,16 @@ python ${prog_dir}EcoFOCIraw2nc.py ${input} ${output}.interpolated.nc sc 0042 -k
 #NetCDF_Trim was combined into NetCDF_Time_Tools --> below shows example of old and new api
 #python ${prog_dir}NetCDF_Trim.py ${output} -sd ${deployment_date} -ed ${recovery_date}
 python ${prog_dir}NetCDF_Time_Tools.py ${output}.interpolated.nc Trim --trim_bounds ${deployment_date} ${recovery_date}
+
+echo "-------------------------------------------------------------"
+echo "SBE37 Processing"
+echo "-------------------------------------------------------------"
+
+#but we said we deployed 1855
+serial_no=1865
+input=${data_dir}${mooringYear}/Moorings/${mooringID}/raw/sbe37/17CKP5Asbe37.asc
+output=${data_dir}${mooringYear}/Moorings/${mooringID}/working/17ckp5a_s37_0041m.unqcd.nc
+python ${prog_dir}EcoFOCIraw2nc.py ${input} ${output} s37 0041 -kw True -latlon $lat $lon -add_meta $mooringID $serial_no $site_depth
+#NetCDF_Trim was combined into NetCDF_Time_Tools --> below shows example of old and new api
+#python ${prog_dir}NetCDF_Trim.py ${output} -sd ${deployment_date} -ed ${recovery_date}
+python ${prog_dir}NetCDF_Time_Tools.py ${output} Trim --trim_bounds ${deployment_date} ${recovery_date}
